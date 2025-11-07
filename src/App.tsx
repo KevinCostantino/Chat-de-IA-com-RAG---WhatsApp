e3cb85ac5d0c965c38e86e45fa9630673d95d56e
KevinCostantino <kevincostantino2500@gmail.com>
2025-11-07 18:38:34 -0300

    [FIX] Corrigir erros TypeScript para deploy Vercel - Remove importações não utilizadas do React

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import ConfigPanel from './components/ConfigPanel'
 import Chat from './components/Chat'
 import DocumentManager from './components/DocumentManager'
